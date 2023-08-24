d6f4fe6a584dabaa64772fa0ce5ce5830ea765ad
ippankbro <irfanarfah.bigtv@gmail.com>
2023-08-24 16:56:33 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <h1>Game Cocoki</h1>
+      <div className="row">
+        <div className="col satu">1</div>
+        <div className="col">2</div>
+        <div className="col">4</div>
+        <div className="col">1</div>
+        <div className="col">4</div>
+        <div className="col">3</div>
+        <div className="col">2</div>
+        <div className="col">3</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
